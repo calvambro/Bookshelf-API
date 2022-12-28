e05486f00802bad44d16f48132d18d74d96846cb
ambrosius calvin <calvambro@gmail.com>
2022-12-28 21:18:28 +0700

    prob 3

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {getAllBooksHandler,
+  addBookHandler,
+  getBookByIdHandler} = require('./handler');
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
